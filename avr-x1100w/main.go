af79bda9267ad6b4788ea3a587588f164732c025
Michael Stapelberg <michael@stapelberg.de>
2019-07-29 17:13:47 +0200

    remove old AVR config

--- avr-x1100w/main.go
+++ avr-x1100w/main.go
@@ -24,20 +24,6 @@ var (
 	midnaURL = flag.String("midna_url",
 		"http://midna:4000/",
 		"URL on which runstatus.go is accessible")
-
-	subwooferLevel = map[string]int{
-		"MPLAY": 50, // PC
-		"BD":    38, // beast
-		"GAME":  38, // chromecast
-		"AUX1":  50, // chromecast audio
-	}
-
-	volume = map[string]int{
-		"MPLAY": 60, // PC
-		"BD":    60, // beast
-		"GAME":  60, // chromecast
-		"AUX1":  60, // chromecast audio
-	}
 )
 
 type State struct {
