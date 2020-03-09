3f5eb432164d463a420d345790cd88e9bef578b9
Michael Stapelberg <michael@stapelberg.de>
2020-03-09 09:44:14 +0100

    runstatus: use netlink proc connector to find i3lock
    This consumes less CPU time than polling /proc every second, but requires root
    privileges.

--- go.mod
+++ go.mod
@@ -5,17 +5,19 @@ go 1.12
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20161002113705-648efa622239 // indirect
 	github.com/eclipse/paho.mqtt.golang v1.1.1
+	github.com/fearful-symmetry/garlic v0.3.0
+	github.com/flynn/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
 	github.com/gliderlabs/ssh v0.2.2
 	github.com/gokrazy/gokrazy v0.0.0-20190321081644-520b8ca41de7
 	github.com/gokrazy/internal v0.0.0-20190630091051-de21a662e434 // indirect
 	github.com/golang/protobuf v1.2.0
+	github.com/mdlayher/netlink v1.1.0 // indirect
 	github.com/micro/mdns v0.1.0
-	github.com/neko-neko/utmpdump v1.0.1-0.20170404040331-e015f7dff491
 	github.com/prometheus/client_golang v0.9.2
 	github.com/vishvananda/netlink v1.0.0
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df // indirect
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
-	golang.org/x/net v0.0.0-20190311183353-d8887717615a
+	golang.org/x/net v0.0.0-20200301022130-244492dfa37a
 	golang.org/x/sync v0.0.0-20190423024810-112230192c58
-	golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a
+	golang.org/x/sys v0.0.0-20200302150141-5c8b2ff67527
 )
--- runstatus/runstatus.go
+++ runstatus/runstatus.go
@@ -11,6 +11,9 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/fearful-symmetry/garlic"
+	"golang.org/x/sync/errgroup"
 )
 
 var (
@@ -26,39 +29,94 @@ var (
 	runMu     sync.RWMutex
 )
 
-func main() {
-	flag.Parse()
-	go func() {
-		for {
-			status := "notrunning"
-			filepath.Walk("/proc", func(path string, info os.FileInfo, err error) error {
-				if path == "/proc" {
-					return nil
-				}
-				if b, err := ioutil.ReadFile(filepath.Join(path, "comm")); err == nil {
+func listenNetlink() error {
+	// Only superuser is allowed to listen to multicast connector messages:
+	// https://github.com/torvalds/linux/blob/2c523b344dfa65a3738e7039832044aa133c75fb/net/netlink/af_netlink.c#L992
+	conn, err := garlic.DialPCNWithEvents([]garlic.EventType{
+		garlic.ProcEventExec,
+		garlic.ProcEventExit,
+	})
+	if err != nil {
+		return err
+	}
+	var prev string
+	programPids := make(map[uint32]bool)
+	for {
+		data, err := conn.ReadPCN()
+		if err != nil {
+			return err
+		}
+		for _, ev := range data {
+			switch x := ev.EventData.(type) {
+			case garlic.Exec:
+				//log.Printf("  exec: %+v", x)
+				if b, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/comm", x.ProcessPid)); err == nil {
 					if strings.TrimSpace(string(b)) == *programName {
-						status = "running"
+						programPids[x.ProcessPid] = true
 					}
 				}
 
-				if err != nil || info.IsDir() {
-					return filepath.SkipDir
-				}
-				return nil
-			})
-
-			runMu.Lock()
-			runStatus = status
-			runMu.Unlock()
-
-			time.Sleep(1 * time.Second)
+			case garlic.Exit:
+				//log.Printf("  exit: %+v", x)
+				delete(programPids, x.ProcessPid)
+			}
 		}
-	}()
+		runMu.Lock()
+		if len(programPids) > 0 {
+			runStatus = "running"
+		} else {
+			runStatus = "notrunning"
+		}
+		if prev != runStatus {
+			log.Printf("  status change: prev=%v, now=%v", prev, runStatus)
+			prev = runStatus
+		}
+		runMu.Unlock()
+	}
+}
+
+func pollProc() error {
+	for {
+		status := "notrunning"
+		filepath.Walk("/proc", func(path string, info os.FileInfo, err error) error {
+			if path == "/proc" {
+				return nil
+			}
+			if b, err := ioutil.ReadFile(filepath.Join(path, "comm")); err == nil {
+				if strings.TrimSpace(string(b)) == *programName {
+					status = "running"
+				}
+			}
+
+			if err != nil || info.IsDir() {
+				return filepath.SkipDir
+			}
+			return nil
+		})
+
+		runMu.Lock()
+		runStatus = status
+		runMu.Unlock()
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	var eg errgroup.Group
+	eg.Go(listenNetlink)
+	// eg.Go(pollProc)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		runMu.RLock()
 		status := runStatus
 		runMu.RUnlock()
 		fmt.Fprintf(w, "%s", status)
 	})
-	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+	eg.Go(func() error { return http.ListenAndServe(*listenAddress, nil) })
+	if err := eg.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
