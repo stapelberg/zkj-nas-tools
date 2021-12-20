f4bff20e9213a216770f808120a774277c1be5a8
Michael Stapelberg <michael@stapelberg.de>
2021-12-20 18:09:02 +0100

    sshutil: route tailscale requests via SOCKS5 proxy
    This should make tailscale and non-tailscale targets work.

--- dornroeschen/sshutil.go
+++ dornroeschen/sshutil.go
@@ -8,10 +8,12 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 
 	"github.com/stapelberg/rsyncprom"
 	"golang.org/x/crypto/ssh"
+	"golang.org/x/net/proxy"
 )
 
 type keychain struct {
@@ -45,9 +47,24 @@ func openSshClientAuth(path string) (ssh.AuthMethod, error) {
 }
 
 func newSshConnection(host, keypath string) (*ssh.Client, error) {
+	// https://tailscale.com/kb/1015/100.x-addresses/
+	_, tailscaleNet, err := net.ParseCIDR("100.64.0.0/10")
+	if err != nil {
+		return nil, err
+	}
+
+	// tailscaled is running with --socks5-server=localhost:1055
+	tsProxied, err := proxy.SOCKS5("tcp", "localhost:1055", nil, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	tsdialer := proxy.NewPerHost(proxy.Direct, tsProxied)
+	tsdialer.AddNetwork(tailscaleNet)
+
 	clientauth, err := openSshClientAuth(keypath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	clientConfig := &ssh.ClientConfig{
@@ -58,11 +75,17 @@ func newSshConnection(host, keypath string) (*ssh.Client, error) {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
 
-	client, err := ssh.Dial("tcp", host+":22", clientConfig)
+	addr := host + ":22"
+	conn, err := tsdialer.Dial("tcp", addr)
 	if err != nil {
 		return nil, err
 	}
-	return client, nil
+
+	c, chans, reqs, err := ssh.NewClientConn(conn, addr, clientConfig)
+	if err != nil {
+		return nil, err
+	}
+	return ssh.NewClient(c, chans, reqs), nil
 }
 
 func newLogWriter(logger *log.Logger) io.Writer {
