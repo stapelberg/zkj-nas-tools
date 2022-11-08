a65879bc497afe8e86d6024ee6784835a890384c
Michael Stapelberg <michael@stapelberg.de>
2022-11-08 21:02:15 +0100

    wake: add midna WOL-only MAC address, require host arg

--- wake/wake.go
+++ wake/wake.go
@@ -46,6 +46,7 @@ func wakeUp(mqttBroker, host, ip, mac string) error {
 		if err := wake.PollSSH(ctx, ip+":22"); err != nil {
 			return err
 		}
+		log.Printf("host %s now awake", host)
 	}
 
 	return nil
@@ -53,24 +54,28 @@ func wakeUp(mqttBroker, host, ip, mac string) error {
 
 func main() {
 	var (
-		host = flag.String("host",
-			"storage2",
-			"which host to wake up (storage2, storage3)")
-
 		mqttBroker = flag.String("mqtt_broker",
 			"tcp://dr.lan:1883",
 			"MQTT broker address for github.com/eclipse/paho.mqtt.golang")
 	)
 	flag.Parse()
+	if flag.NArg() != 1 {
+		log.Fatalf("syntax: wake <storage2|storage3|midna>")
+	}
+	host := flag.Arg(0)
 	ips := map[string]string{
 		"storage2": "10.0.0.252",
 		"storage3": "10.0.0.253",
+		"midna":    "10.0.0.76",
 	}
 	macs := map[string]string{
 		// storage2 is woken up via MQTT
 		"storage3": "70:85:c2:b6:02:24",
+		// On-board network card connected for WOL only (link not even up in
+		// Linux).
+		"midna": "04:42:1a:31:9e:97",
 	}
-	if err := wakeUp(*mqttBroker, *host, ips[*host], macs[*host]); err != nil {
+	if err := wakeUp(*mqttBroker, host, ips[host], macs[host]); err != nil {
 		log.Fatal(err)
 	}
 }
